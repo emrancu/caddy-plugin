6c192f85fdf157a918f31366ac8a53956d4fc827
AL EMRAN <emrancu1@gmail.com>
2023-07-05 00:05:36 +0600

    u

--- main.go
+++ main.go
@@ -1,46 +1,29 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
+	"github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
-	"github.com/caddyserver/caddy/v2/modules/caddyhttp/caddylocale"
 )
 
-func init() {
-	caddyhttp.RegisterModule(ExtraString{})
-	caddyhttp.RegisterModule(caddylocale.Localization{})
-	caddyhttp.RegisterHandlerConstructor("extra_string", parseExtraString)
+type CustomHeader struct {
+	Name  string `json:"name,omitempty"`
+	Value string `json:"value,omitempty"`
 }
 
-// ExtraString represents the custom handler that adds an extra string to the response body.
-type ExtraString struct {
-	Next   caddyhttp.Handler
-	String string `json:"string,omitempty"`
+func (h CustomHeader) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	w.Header().Add(h.Name, h.Value)
+	return next.ServeHTTP(w, r)
 }
 
-// ServeHTTP implements the caddyhttp.Handler interface.
-func (es ExtraString) ServeHTTP(w http.ResponseWriter, r *http.Request) error {
-	rec := w.(caddyhttp.ResponseWriter)
-	rec.Header().Add("X-Extra-String", es.String)
-	return es.Next.ServeHTTP(rec, r)
-}
-
-// parseExtraString creates a handler instance based on the provided config.
-func parseExtraString(h caddyhttp.Helper) (caddyhttp.Handler, error) {
-	var es ExtraString
-	err := es.UnmarshalCaddyfile(h.Dispenser)
-	return es, err
-}
-
-// UnmarshalCaddyfile sets up the handler from Caddyfile tokens.
-func (es *ExtraString) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
-	for d.Next() {
-		if d.NextArg() {
-			es.String = d.Val()
-		}
+func CustomHeaderDirective() caddyhttp.MiddlewareHandler {
+	return func(next caddyhttp.Handler) caddyhttp.Handler {
+		return &CustomHeader{}
 	}
-	return nil
+}
+
+func init() {
+	caddy.RegisterModule(&CustomHeader{})
+	caddy.RegisterModule(CustomHeaderDirective)
 }
